0a58c67301baab8a2fe1a32e7c2f300a65ebb225
niv <n@e-ix.net>
2017-02-15 20:28:11 +0100

    add missing languages.nim. derp.

---
+++ src/neverwinter/languages.nim
@@ -0,0 +1,16 @@
+type
+  Language* {.pure.} = enum
+    English = 0
+    French = 1
+    German = 2
+    Italian = 3
+    Spanish = 4
+    Polish = 5
+    Korean = 128
+    ChineseTraditional = 129
+    ChineseSimplified = 130
+    Japanese = 131
+
+  Gender* {.pure.} = enum
+    Male,
+    Female

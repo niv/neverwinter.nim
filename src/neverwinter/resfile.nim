2601576ce0010e97b111d5bb82f4f936e02dea40
niv <niv@nwnx.io>
2017-02-22 15:29:59 +0100

    ResFile: immediately resolve named resref, add support for count & contents

--- src/neverwinter/resfile.nim
+++ src/neverwinter/resfile.nim
@@ -7,10 +7,12 @@ import resman, util
 type
   ResFile* = ref object of ResContainer
     filename: string
+    resRef: ResolvedResRef
 
 proc newResFile*(filename: string): ResFile =
   new(result)
   result.filename = filename
+  result.resRef = newResolvedResRef(filename)
 
 proc resRefToFullPath(self: ResFile, rr: ResolvedResRef): string = rr.toFile
 
@@ -23,4 +25,10 @@ method demand*(self: ResFile, rr: ResRef): Res =
   let mtime = getLastModificationTime(fp)
   result = newRes(rr, mtime, newFileStream(fp))
 
-method count*(self: ResFile): int = 1 # TODO: check for access/existence
+method count*(self: ResFile): int = # TODO: check for access/existence
+  if fileExists(self.resRefToFullPath(self.resRef)): 1 else: 0
+
+method contents*(self: ResFile): HashSet[ResRef] =
+  result = initSet[ResRef]()
+  if fileExists(self.resRefToFullPath(self.resRef)):
+    result.incl(self.resRef)

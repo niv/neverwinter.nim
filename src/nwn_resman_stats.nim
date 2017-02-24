ad123b77561f1790a21be74e094fa190122d6b63
niv <niv@nwnx.io>
2017-02-24 14:26:23 +0100

    utility: nwn_resman_stats

---
+++ src/nwn_resman_stats.nim
@@ -0,0 +1,29 @@
+import shared
+let Args = DOC """
+This utility gives you some general statistics on a resman view.  This only
+includes files seen by resman, not shadowed content.
+
+Usage:
+  $0 [options]
+
+$OPT
+"""
+
+let rm = newBasicResMan(findNwnRoot())
+
+var count = initCountTable[ResType]()
+var size  = initTable[ResType, int64]()
+
+for o in rm.contents.withProgressBar("index: "):
+  count.inc(o.resType)
+  if not size.hasKey o.resType: size[o.resType] = 0
+  let retr = rm[o].get()
+
+  doAssert(retr.len >= 0, $retr & " size is " & $retr.len)
+
+  size[o.resType] += retr.len
+
+count.sort
+
+for k, v in count:
+  echo k, "\t", v, "\t", size[k].formatSize

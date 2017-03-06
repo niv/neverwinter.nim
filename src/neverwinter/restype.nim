1f2a34f10c3884b68b678a2d9535fdee0082bd1e
niv <niv@nwnx.io>
2017-03-06 21:17:25 +0100

    Restype: make distinct

--- src/neverwinter/restype.nim
+++ src/neverwinter/restype.nim
@@ -8,8 +8,9 @@ import tables, strutils, options
 
 import util
 
-type
-  ResType* = uint16
+type ResType* = distinct uint16
+
+proc `==`*(x, y: ResType): bool {.borrow.}
 
 var types  = initTable[ResType, string]()
 var rtypes = initTable[string, ResType]()
@@ -31,6 +32,12 @@ proc lookupResExt*(resType: ResType): Option[string] =
   if types.hasKey(resType):
     result = some(types[resType])
 
+proc getResType*(extension: string): ResType =
+  lookupResType(extension).get()
+
+proc getResExt*(resType: ResType): string =
+  lookupResExt(resType).get()
+
 proc `$`*(r: ResType): string =
   proc IntToStr(x: int): string {.magic: "IntToStr", noSideEffect.}
   let ext = lookupResExt(r)
@@ -38,81 +45,81 @@ proc `$`*(r: ResType): string =
   else: $r.int
 
 # nwn1
-registerResType(1, "bmp");
-registerResType(2, "mve");
-registerResType(3, "tga");
-registerResType(4, "wav");
-registerResType(6, "plt");
-registerResType(7, "ini");
-registerResType(8, "bmu");
-registerResType(9, "mpg");
-registerResType(10, "txt");
-registerResType(2000, "plh");
-registerResType(2001, "tex");
-registerResType(2002, "mdl");
-registerResType(2003, "thg");
-registerResType(2005, "fnt");
-registerResType(2007, "lua");
-registerResType(2008, "slt");
-registerResType(2009, "nss");
-registerResType(2010, "ncs");
-registerResType(2011, "mod");
-registerResType(2012, "are");
-registerResType(2013, "set");
-registerResType(2014, "ifo");
-registerResType(2015, "bic");
-registerResType(2016, "wok");
-registerResType(2017, "2da");
-registerResType(2018, "tlk");
-registerResType(2022, "txi");
-registerResType(2023, "git");
-registerResType(2024, "bti");
-registerResType(2025, "uti");
-registerResType(2026, "btc");
-registerResType(2027, "utc");
-registerResType(2029, "dlg");
-registerResType(2030, "itp");
-registerResType(2031, "btt");
-registerResType(2032, "utt");
-registerResType(2033, "dds");
-registerResType(2034, "bts");
-registerResType(2035, "uts");
-registerResType(2036, "ltr");
-registerResType(2037, "gff");
-registerResType(2038, "fac");
-registerResType(2039, "bte");
-registerResType(2040, "ute");
-registerResType(2041, "btd");
-registerResType(2042, "utd");
-registerResType(2043, "btp");
-registerResType(2044, "utp");
-registerResType(2045, "dft");
-registerResType(2046, "gic");
-registerResType(2047, "gui");
-registerResType(2048, "css");
-registerResType(2049, "ccs");
-registerResType(2050, "btm");
-registerResType(2051, "utm");
-registerResType(2052, "dwk");
-registerResType(2053, "pwk");
-registerResType(2054, "btg");
-registerResType(2055, "utg");
-registerResType(2056, "jrl");
-registerResType(2057, "sav");
-registerResType(2058, "utw");
-registerResType(2059, "4pc");
-registerResType(2060, "ssf");
-registerResType(2061, "hak");
-registerResType(2062, "nwm");
-registerResType(2063, "bik");
-registerResType(2064, "ndb");
-registerResType(2065, "ptm");
-registerResType(2066, "ptt");
-registerResType(2067, "bak");
-registerResType(2068, "dat");
-registerResType(2069, "shd");
-registerResType(2070, "xbc");
-registerResType(9996, "ids");
-registerResType(9997, "erf");
-registerResType(9998, "bif");
-registerResType(9999, "key");
+registerResType(ResType 1, "bmp");
+registerResType(ResType 2, "mve");
+registerResType(ResType 3, "tga");
+registerResType(ResType 4, "wav");
+registerResType(ResType 6, "plt");
+registerResType(ResType 7, "ini");
+registerResType(ResType 8, "bmu");
+registerResType(ResType 9, "mpg");
+registerResType(ResType 10, "txt");
+registerResType(ResType 2000, "plh");
+registerResType(ResType 2001, "tex");
+registerResType(ResType 2002, "mdl");
+registerResType(ResType 2003, "thg");
+registerResType(ResType 2005, "fnt");
+registerResType(ResType 2007, "lua");
+registerResType(ResType 2008, "slt");
+registerResType(ResType 2009, "nss");
+registerResType(ResType 2010, "ncs");
+registerResType(ResType 2011, "mod");
+registerResType(ResType 2012, "are");
+registerResType(ResType 2013, "set");
+registerResType(ResType 2014, "ifo");
+registerResType(ResType 2015, "bic");
+registerResType(ResType 2016, "wok");
+registerResType(ResType 2017, "2da");
+registerResType(ResType 2018, "tlk");
+registerResType(ResType 2022, "txi");
+registerResType(ResType 2023, "git");
+registerResType(ResType 2024, "bti");
+registerResType(ResType 2025, "uti");
+registerResType(ResType 2026, "btc");
+registerResType(ResType 2027, "utc");
+registerResType(ResType 2029, "dlg");
+registerResType(ResType 2030, "itp");
+registerResType(ResType 2031, "btt");
+registerResType(ResType 2032, "utt");
+registerResType(ResType 2033, "dds");
+registerResType(ResType 2034, "bts");
+registerResType(ResType 2035, "uts");
+registerResType(ResType 2036, "ltr");
+registerResType(ResType 2037, "gff");
+registerResType(ResType 2038, "fac");
+registerResType(ResType 2039, "bte");
+registerResType(ResType 2040, "ute");
+registerResType(ResType 2041, "btd");
+registerResType(ResType 2042, "utd");
+registerResType(ResType 2043, "btp");
+registerResType(ResType 2044, "utp");
+registerResType(ResType 2045, "dft");
+registerResType(ResType 2046, "gic");
+registerResType(ResType 2047, "gui");
+registerResType(ResType 2048, "css");
+registerResType(ResType 2049, "ccs");
+registerResType(ResType 2050, "btm");
+registerResType(ResType 2051, "utm");
+registerResType(ResType 2052, "dwk");
+registerResType(ResType 2053, "pwk");
+registerResType(ResType 2054, "btg");
+registerResType(ResType 2055, "utg");
+registerResType(ResType 2056, "jrl");
+registerResType(ResType 2057, "sav");
+registerResType(ResType 2058, "utw");
+registerResType(ResType 2059, "4pc");
+registerResType(ResType 2060, "ssf");
+registerResType(ResType 2061, "hak");
+registerResType(ResType 2062, "nwm");
+registerResType(ResType 2063, "bik");
+registerResType(ResType 2064, "ndb");
+registerResType(ResType 2065, "ptm");
+registerResType(ResType 2066, "ptt");
+registerResType(ResType 2067, "bak");
+registerResType(ResType 2068, "dat");
+registerResType(ResType 2069, "shd");
+registerResType(ResType 2070, "xbc");
+registerResType(ResType 9996, "ids");
+registerResType(ResType 9997, "erf");
+registerResType(ResType 9998, "bif");
+registerResType(ResType 9999, "key");

8d646ed7b5b1cfbbf8afe025ab54734a52764189
niv <n@e-ix.net>
2017-04-21 13:39:57 +0200

    tlk writer: never bring newlines into a tlk file

---
+++ tests/test_tlk.nim
@@ -0,0 +1,14 @@
+import unittest, streams
+include neverwinter.tlk
+
+suite "SingleTlk":
+  test "never writes out caret returns in strings":
+    var outstr = newStringStream()
+    var tlk = newSingleTlk()
+    tlk[0] = "test\r\nstring"
+    outstr.write(tlk)
+    outstr.setPosition(0)
+    let rd = outstr.readAll()
+    let offset = rd.find("test")
+    check (offset != -1)
+    check(rd.substr(offset, offset + 10) == "test\nstring")

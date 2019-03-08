514619726dec482394724ce3af0020f01dccefdf
niv <n@e-ix.net>
2019-03-08 12:49:52 +0100

    nwn_twoda: --write-id-column writes IDs to csv, fixes #5

--- nwn_twoda.nim
+++ nwn_twoda.nim
@@ -30,6 +30,14 @@ Options:
   -k OUTFORMAT                Output format [default: autodetect]
 
   --csv-separator SEP         What to use as separator for CSV cells [default: ,]
+
+  --write-id-column           Generate ID column when exporting non-2da formats.
+                              Not all formats might be supported. Note that the ID column
+                              is ignored by the game and the generated column is only
+                              for your convenience.
+                              Thus, when reading files generated with this option, the ID column
+                              is also discarded. IDs are assigned based on order of appearance.
+
   $OPT
 """
 
@@ -49,13 +57,16 @@ proc readCsv(s: Stream): TwoDA =
   var p: CsvParser
   p.open(input = s, filename = inputfile, separator = ($args["--csv-separator"])[0])
   p.readHeaderRow()
-  result.columns = p.headers
+  let haveid = p.headers[0].toLowerAscii() == "id"
+  result.columns = if haveid: p.headers[1..<p.headers.len] else: p.headers
   while p.readRow():
-    result.rows.add(p.row.mapIt(some it))
+    let row = if haveId: p.row[1..<p.row.len] else: p.row
+    result.rows.add(row.mapIt(some it))
 
   p.close()
 
 proc writeCsv(s: Stream, tbl: TwoDA) =
+  let writeid = args["--write-id-column"]
   proc joinRow(r: Row): string =
     proc quoteOrStar(s: Cell): string =
       if s.isSome:
@@ -67,8 +78,13 @@ proc writeCsv(s: Stream, tbl: TwoDA) =
         "****"
     r.mapIt(it.quoteOrStar).join(($args["--csv-separator"])[0..0])
 
-  s.writeLine(tbl.columns.mapIt(some it).joinRow())
-  for r in tbl.rows: s.writeLine(r.joinRow())
+  let columns = if writeid: concat(@["ID"], tbl.columns)
+                else: tbl.columns
+
+  s.writeLine(columns.mapIt(some it).joinRow())
+  for idx, r in tbl.rows:
+    let row = if writeid: concat(@[some $idx], r) else: r
+    s.writeLine(row.joinRow())
 
 var state: TwoDA
 

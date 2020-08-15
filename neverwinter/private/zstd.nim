ceea008642f6e73b17613709ab832944b22d6813
niv <n@e-ix.net>
2020-08-15 17:27:21 +0200

    private: add zstd 1.4.5 + bindings

---
+++ neverwinter/private/zstd.nim
@@ -0,0 +1,59 @@
+import neverwinter/private/zstd/binding
+
+const ZSTD_CLEVEL_DEFAULT* = 3
+
+type CompressionContext = ref object of RootObj
+  ctx: ZSTD_CCtx
+
+type DecompressionContext = ref object of RootObj
+  ctx: ZSTD_DCtx
+
+proc newCompressionContext*(): CompressionContext =
+  new(result) do (r: CompressionContext):
+    discard ZSTD_freeCCtx(r.ctx)
+  result.ctx = ZSTD_createCCtx()
+
+proc newDecompressionContext*(): DecompressionContext =
+  new(result) do (r: DecompressionContext):
+    discard ZSTD_freeDCtx(r.ctx)
+  result.ctx = ZSTD_createDCtx()
+
+proc getCompressBound*(inputlen: int): int =
+  ZSTD_compressBound(inputlen.csize_t).int
+
+proc getFrameContentSize*(input: string): int =
+  result = int ZSTD_getFrameContentSize(unsafeAddr(input[0]), input.len.csize_t)
+  if result == -1: raise newException(ValueError, "ContentSize Unknown")
+  if result == -2: raise newException(ValueError, "ContentSize Error")
+
+proc compress*(input: string, level: int = 3, ctx: CompressionContext = nil): string =
+  let strlen = ZSTD_compressBound(input.len.csize_t)
+  if ZSTD_isError(strlen) > 0:
+    raise newException(ValueError, $ZSTD_getErrorName(strlen))
+  result = newString(strlen)
+
+  let err =
+    if not isNil(ctx):
+      ZSTD_compressCCtx(ctx.ctx, addr(result[0]), strlen, unsafeAddr(input[0]), input.len.csize_t, level.cint)
+    else:
+      ZSTD_compress(addr(result[0]), strlen, unsafeAddr(input[0]), input.len.csize_t, level.cint)
+
+  if ZSTD_isError(err) > 0:
+    raise newException(ValueError, $ZSTD_getErrorName(err))
+  result.setLen(err)
+
+proc decompress*(input: string, ctx: DecompressionContext = nil): string =
+  let strlen = ZSTD_getFrameContentSize(unsafeAddr(input[0]), input.len.csize_t).csize_t
+  if ZSTD_isError(strlen) > 0:
+    raise newException(ValueError, $ZSTD_getErrorName(strlen))
+  result = newString(strlen)
+
+  let err =
+    if not isNil(ctx):
+      ZSTD_decompressDCtx(ctx.ctx, addr(result[0]), strlen, unsafeAddr(input[0]), input.len.csize_t)
+    else:
+      ZSTD_decompress(addr(result[0]), strlen, unsafeAddr(input[0]), input.len.csize_t)
+
+  if ZSTD_isError(err) > 0:
+    raise newException(ValueError, $ZSTD_getErrorName(err))
+  result.setLen(err)
---
+++ neverwinter/private/zstd/binding.nim
@@ -0,0 +1,89 @@
+
+{.passC: "-DZSTD_STATIC_LINKING_ONLY".}
+
+from os import splitPath
+const sourceRoot = currentSourcePath().splitPath.head
+
+{.passC: "-I" & sourceRoot & "/lib/common".}
+{.passC: "-I" & sourceRoot & "/lib/decompress".}
+{.passC: "-I" & sourceRoot & "/lib/compress".}
+{.passC: "-I" & sourceRoot & "/lib/dictBuilder".}
+{.passC: "-I" & sourceRoot & "/lib/legacy".}
+
+{.compile: sourceRoot & "/lib/compress/zstd_compress_superblock.c".}
+{.compile: sourceRoot & "/lib/compress/zstdmt_compress.c".}
+{.compile: sourceRoot & "/lib/compress/zstd_double_fast.c".}
+{.compile: sourceRoot & "/lib/compress/zstd_fast.c".}
+{.compile: sourceRoot & "/lib/compress/zstd_compress_sequences.c".}
+{.compile: sourceRoot & "/lib/compress/zstd_ldm.c".}
+{.compile: sourceRoot & "/lib/compress/hist.c".}
+{.compile: sourceRoot & "/lib/compress/zstd_compress.c".}
+{.compile: sourceRoot & "/lib/compress/zstd_lazy.c".}
+{.compile: sourceRoot & "/lib/compress/zstd_compress_literals.c".}
+{.compile: sourceRoot & "/lib/compress/huf_compress.c".}
+{.compile: sourceRoot & "/lib/compress/zstd_opt.c".}
+{.compile: sourceRoot & "/lib/compress/fse_compress.c".}
+{.compile: sourceRoot & "/lib/dictBuilder/cover.c".}
+{.compile: sourceRoot & "/lib/dictBuilder/divsufsort.c".}
+{.compile: sourceRoot & "/lib/dictBuilder/fastcover.c".}
+{.compile: sourceRoot & "/lib/dictBuilder/zdict.c".}
+{.compile: sourceRoot & "/lib/decompress/zstd_ddict.c".}
+{.compile: sourceRoot & "/lib/decompress/huf_decompress.c".}
+{.compile: sourceRoot & "/lib/decompress/zstd_decompress.c".}
+{.compile: sourceRoot & "/lib/decompress/zstd_decompress_block.c".}
+{.compile: sourceRoot & "/lib/legacy/zstd_v05.c".}
+{.compile: sourceRoot & "/lib/legacy/zstd_v01.c".}
+{.compile: sourceRoot & "/lib/legacy/zstd_v06.c".}
+{.compile: sourceRoot & "/lib/legacy/zstd_v02.c".}
+{.compile: sourceRoot & "/lib/legacy/zstd_v07.c".}
+{.compile: sourceRoot & "/lib/legacy/zstd_v03.c".}
+{.compile: sourceRoot & "/lib/legacy/zstd_v04.c".}
+{.compile: sourceRoot & "/lib/common/entropy_common.c".}
+{.compile: sourceRoot & "/lib/common/fse_decompress.c".}
+{.compile: sourceRoot & "/lib/common/debug.c".}
+{.compile: sourceRoot & "/lib/common/xxhash.c".}
+{.compile: sourceRoot & "/lib/common/pool.c".}
+{.compile: sourceRoot & "/lib/common/threading.c".}
+{.compile: sourceRoot & "/lib/common/zstd_common.c".}
+{.compile: sourceRoot & "/lib/common/error_private.c".}
+
+# compat shim for 1.0.x
+when not compiles(csize_t):
+  type csize_t* = csize
+
+proc ZSTD_versionString*(): cstring {.importc.}
+proc ZSTD_versionNumber*(): cuint {.importc.}
+
+proc ZSTD_isError*(srcsize: csize_t): csize_t {.importc.}
+proc ZSTD_getErrorName*(srcsize: csize_t): cstring {.importc.}
+
+proc ZSTD_compressBound*(srcsize: csize_t): csize_t {.importc.}
+
+proc ZSTD_compress*(
+  dst: pointer, dstCapacity: csize_t,
+  src: pointer, srcsize: csize_t,
+  compressionLevel: cint): csize_t {.importc: "ZSTD_compress".}
+
+proc ZSTD_getFrameContentSize*(src: pointer, srcsize: csize_t): culonglong {.importc.}
+
+proc ZSTD_decompress*(
+  dst: pointer, dstSize: csize_t,
+  src: pointer, compressedSize: csize_t): csize_t {.importc.}
+
+type ZSTD_CCtx* = distinct pointer
+type ZSTD_DCtx* = distinct pointer
+proc ZSTD_createCCtx*(): ZSTD_CCtx {.importc.}
+proc ZSTD_freeCCtx*(ctx: ZSTD_CCtx): cuint {.importc.}
+proc ZSTD_createDCtx*(): ZSTD_DCtx {.importc.}
+proc ZSTD_freeDCtx*(ctx: ZSTD_DCtx): cuint {.importc.}
+
+proc ZSTD_compressCCtx*(
+  ctx: ZSTD_CCtx,
+  dst: pointer, dstCapacity: csize_t,
+  src: pointer, srcsize: csize_t,
+  compressionLevel: cint): csize_t {.importc.}
+
+proc ZSTD_decompressDCtx*(
+  ctx: ZSTD_DCtx,
+  dst: pointer, dstCapacity: csize_t,
+  src: pointer, srcsize: csize_t): csize_t {.importc.}

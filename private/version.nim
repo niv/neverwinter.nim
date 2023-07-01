f042c4064a5b6e337ec5599ef156ed51f4f0e06c
Bernhard Stoeckner <n@e-ix.net>
2023-07-01 11:20:57 +0200

    version: make threadsafe

--- private/version.nim
+++ private/version.nim
@@ -7,12 +7,11 @@ const Licence: string  = slurp(currentSourcePath().splitFile().dir & "/../LICENC
 const GitBranch*: string = staticExec("git symbolic-ref -q --short HEAD").strip
 const GitRev*: string    = staticExec("git rev-parse HEAD").strip
 
-let nimbleConfig        = loadConfig(newStringStream(Nimble))
-let PackageVersion*     = nimbleConfig.getSectionValue("", "version")
-let PackageDescription* = nimbleConfig.getSectionValue("", "description")
-let VersionString*  = "neverwinter " & PackageVersion & " (" & GitBranch & "/" & GitRev[0..5] & ", nim " & NimVersion & ")"
-
 proc printVersion*() =
+  let nimbleConfig       = loadConfig(newStringStream(Nimble))
+  let PackageVersion     = nimbleConfig.getSectionValue("", "version")
+  let VersionString      = "neverwinter " & PackageVersion & " (" & GitBranch & "/" & GitRev[0..5] & ", nim " & NimVersion & ")"
+
   echo Template.
        replace("$LICENCE", Licence).
        replace("$VERSION", VersionString)

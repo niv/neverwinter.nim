f325049920e0f510c012c50599b0c701fd54a6f4
Bernhard Stoeckner <n@e-ix.net>
2023-07-18 10:59:53 +0200

    truntestvm: exercise debug off/on, -O0 and -O2

--- tests/scriptcomp/truntestvm.nim
+++ tests/scriptcomp/truntestvm.nim
@@ -1,4 +1,4 @@
-import std/[streams, strutils, random, os, logging]
+import std/[streams, strutils, random, os, logging, options, enumutils, sequtils, setutils]
 import neverwinter/nwscript/[nwtestvm, compiler, ndb]
 import neverwinter/[restype, resfile, resdir, resman]
 
@@ -12,16 +12,13 @@ rm.add newResDir(SourcePath / "corpus")
 
 const LangSpecNWTestScript = ("nwtestvmscript", ResType 2009, ResType 2010, ResType 2064).LangSpec
 
-# Note: This test also exercises the NDB parser.
-const DebugSymbols = true
-
 const NCSHeaderSize = 13
 
 var currentFile: string
 var currentLines: seq[string]
-var currentDebug: NDB
+var currentDebug: Option[NDB]
 
-let cNSS = newCompiler(LangSpecNWTestScript, DebugSymbols, rm)
+let cNSS = newCompiler(LangSpecNWTestScript, true, rm)
 
 var vm: VM
 new(vm)
@@ -40,13 +37,15 @@ vm.defineCommand(Assert.int) do (script: VMScript):
   # attempt to find the failing line in ndb.
   var loc = ""
 
-  for l in currentDebug.lines:
-    if (NCSHeaderSize + script.ip.uint32) in l.bStart..l.bEnd:
-      let ext = getResExt(LangSpecNWTestScript.src)
-      loc = format("$#.$#:$#: ", currentDebug.files[l.fileNum], ext, l.lineNum)
-      if currentFile == currentDebug.files[l.fileNum]:
-        msg = msg & " (`" & currentLines[l.lineNum.int - 1].strip & "`)"
-      break
+  if currentDebug.isSome:
+    var debug = currentDebug.get()
+    for l in debug.lines:
+      if (NCSHeaderSize + script.ip.uint32) in l.bStart..l.bEnd:
+        let ext = getResExt(LangSpecNWTestScript.src)
+        loc = format("$#.$#:$#: ", debug.files[l.fileNum], ext, l.lineNum)
+        if currentFile == debug.files[l.fileNum]:
+          msg = msg & " (`" & currentLines[l.lineNum.int - 1].strip & "`)"
+        break
 
   doAssert boo, format("[ip=$#,sp=$#] $#$#", script.ip, script.sp, loc, msg)
 
@@ -62,10 +61,12 @@ vm.defineCommand(TakeInt.int) do (script: VMScript):
 vm.defineCommand(TakeClosure.int) do (script: VMScript):
   discard
 
-var scriptsRan = 0
-
-for file in walkFiles(SourcePath / "corpus" / "*.nss"):
+proc testFileSingle(file: string, debugSymbols: bool, optFlags: set[OptimizationFlag]) =
   let ff = splitFile(file).name
+
+  currentLines = splitLines(readFile(file))
+  currentFile  = ff
+  currentDebug = none(NDB)
 
   let expectCode = block:
     var expectStr = readLines(file, 1)[0]
@@ -75,7 +76,9 @@ for file in walkFiles(SourcePath / "corpus" / "*.nss"):
     else:
       0
 
-  info "Compiling: ", ff
+  info format("Compiling $# [debug: $#, opt: $#]", ff, debugSymbols, optFlags)
+  setGenerateDebuggerOutput(cNSS, debugSymbols)
+  setOptimizations(cNSS, optFlags)
   let ret = cNSS.compileFile(ff)
 
   doAssert ret.code == expectCode,
@@ -84,20 +87,29 @@ for file in walkFiles(SourcePath / "corpus" / "*.nss"):
   # If we expected the file not to compile, continue. The expectations are checked above.
   if expectCode != 0:
     info "Compile code ", expectCode, " - expected failure, next"
-    inc scriptsRan
-    continue
+    return
 
   doAssert ret.bytecode != ""
-  doAssert ret.debugcode != ""
 
-  # for Assert()
-  currentLines = splitLines(readFile(file))
-  currentDebug = parseNdb(newStringStream ret.debugcode)
-  currentFile = ff
+  if debugSymbols:
+    doAssert ret.debugcode != "", "expected debug output, but none was generated"
+
+    # for Assert()
+    currentDebug = some parseNdb(newStringStream ret.debugcode)
 
   var script = newVMScript(vm, ret.bytecode, ff)
   info "Running: ", ff
   run script
-  inc scriptsRan
+
+# =======
+
+var scriptsRan = 0
+
+for optFlags in [OptimizationFlagsO0, OptimizationFlagsO2]:
+  for dbg in [false, true]:
+    for file in walkFiles(SourcePath / "corpus" / "*.nss"):
+      testFileSingle(file, dbg, optFlags)
+      inc scriptsRan
 
 doAssert scriptsRan > 0, "test didn't run any scripts?"
+info scriptsRan, " tests completed OK"

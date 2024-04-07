b8f38e0c4b032854747ec2d374ca5f9ec7936184
Bernhard Stoeckner <n@e-ix.net>
2024-04-07 10:12:01 +0200

    sha1: better error message on parse failure

--- neverwinter/checksums.nim
+++ neverwinter/checksums.nim
@@ -1,10 +1,16 @@
 import checksums/[sha1, md5]
 
-from std/strutils import toLowerAscii
+from std/strutils import toLowerAscii, escape
 
-export sha1 except `$`
+export sha1 except `$`, parseSecureHash
 export md5
 
 # Bit of a hack, sorry: always print securehash in lowercase
 proc `$`*(s: SecureHash): string =
   toLowerAscii(sha1.`$`(s))
+
+proc parseSecureHash*(s: string): SecureHash =
+  try:
+    sha1.parseSecureHash(s)
+  except:
+    raise newException(ValueError, "Not a valid SHA1: " & s.escape())

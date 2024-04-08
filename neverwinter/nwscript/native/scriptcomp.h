fc080d6ee79ace76e9a93058711c5eff38a32695
Milos Tijanic <sherincall@gmail.com>
2024-04-08 22:56:21 +0200

    Add more useful info when triggering parser errors (#125)
    There's relatively few errors and many paths in which they can be
    generated, so it's not at all clear why an error happened. This change
    adds some more info to the error print.
    
    The info is mostly useful for people who are somewhat familiar with the
    codebase, but hopefully the extra info helps the average user diagnose
    the issue, or at least doesn't scare them too much.
    
    I'd be fine with putting this behind a toggle or an env var or something
    too. I just don't want to carry the patch around every time I want to
    debug someone's issue.
    
    (I was gonna add @Daztek to review as well since I think he does
    something similar manually, but github won't let me)
    
    ## Testing
    ```C
    void main() {
        fakefunction("fake");
    }
    ```
    triggers this output:
    ```
    F [2024-04-07T23:35:56] [1/1] test.nss: test.nss(2): ERROR: UNDEFINED IDENTIFIER (fakefunction) [WITHIN_A_STATEMENT:11:2:LEFT_BRACKET] [<0ms]
    ```
    Tried a few others as well.
    
    ## Changelog
    
    ### Changed
    - scriptcomp: The compiler now prints additional info on error messages.
    ## Licence
    
    - [x] I am licencing my change under the project's MIT licence,
    including all changes to GPL-3.0 licenced parts of the codebase.

--- neverwinter/nwscript/native/scriptcomp.h
+++ neverwinter/nwscript/native/scriptcomp.h
@@ -550,6 +550,7 @@ private:
 	int32_t m_nSwitchStackDepth;
 
 	CExoString m_sUndefinedIdentifier;
+	CExoString m_sParserErrorExtraInfo;
 
 	BOOL m_bSwitchLabelDefault;
 	int32_t  m_nSwitchLabelNumber;
